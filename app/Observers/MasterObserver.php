c26d1814a39fa3f571922135c49c50678ad505f7
Olexandr <olexandr.develop@gmail.com>
2026-07-07 13:52:32 +0200

    Replace config-based admin allowlists with is_admin database column

--- app/Observers/MasterObserver.php
+++ app/Observers/MasterObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Enums\AppBrand;
 use App\Http\Services\Master\MasterService;
 use App\Models\Master;
 
@@ -17,7 +18,29 @@ class MasterObserver
 
     public function creating(Master $master): void
     {
-        $master->slug = MasterService::generateSlug($master);
+        $master->slug = $this->uniqueSlug(MasterService::generateSlug($master), $master->app);
+    }
+
+    /**
+     * Two masters (e.g. different branches of the same chain) can generate the same
+     * base slug from name+specialty; masters_slug_app_unique then rejects the insert.
+     * Append -2, -3, ... until the slug is free within the brand.
+     */
+    protected function uniqueSlug(string $baseSlug, ?string $app): string
+    {
+        $brand = $app ?: (config('app.client') instanceof AppBrand
+            ? config('app.client')->value
+            : (config('app.client') ?: AppBrand::CARBEAT->value));
+
+        $slug = $baseSlug;
+        $suffix = 2;
+
+        while (Master::withoutGlobalScope('app')->where('app', $brand)->where('slug', $slug)->exists()) {
+            $slug = $baseSlug.'-'.$suffix;
+            $suffix++;
+        }
+
+        return $slug;
     }
 
     /**
